17d41fb25f6d815a80428468e0061940e7c26d42
Vinishanaik <111833630+Vinishanaik@users.noreply.github.com>
2022-10-13 12:00:17 +0530

    Add files via upload

---
+++ Test/Meeting_reservation.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Meeting_Room_Booking.Pages.Test
+{
+    public class Meeting_reservationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Test/contact.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Meeting_Room_Booking.Pages.Test
+{
+    public class contactModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
